1c7935a25dcaaf0df4009e71080447559060c366
Yassine Youssef <youssef.yassine@theopentag.com>
2022-06-05 10:26:46 +0300

    fix photo parsing in product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,47 +1,39 @@
-const Product = require('../models/product'); 
-const fs = require('fs')
+const Product = require('../models/product');
+const fs = require('fs');
 
 const formidable = require('formidable');
 
 exports.createProduct = (req, res) => {
-    
-let form = new formidable.IncomingForm(); 
+  const form = new formidable.IncomingForm();
 
-form.keepExtensions = true; 
+  form.keepExtensions = true;
 
-form.parse(req,(err, fields, files) => {
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Image could not upload!',
+      });
+    }
 
-    if(err) {
+    const product = new Product(fields);
 
+    if (files.photo) {
+      const photoBuffer = {
+        data: fs.readFileSync(files.photo.filepath),
+        contentType: files.photo.mimetype,
+      };
+      product.photo = photoBuffer;
+    }
+
+    product.save((err, product) => {
+      if (err) {
         return res.status(400).json({
-
-            error: 'Image could not upload!'
-        })
-    }
-
-    let product = new Product(fields);
-
-    if(fields.photo){
-        product.photo.data = fs.readFileSync(files.photo.path)
-        product.photo.contentType = files.photo.type
-    }
-
-    product.save((err, product) => { 
-        if(err){
-            return res.status(400).json({
- 
-                err : 'Product not presisted'
-
-            })
-        }
-        res.json({
-
-          product
-
-
-          })
-      })
-   })
-
-
-}
+          err,
+        });
+      }
+      res.json({
+        product,
+      });
+    });
+  });
+};
