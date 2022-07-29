9dc29d22b48ba210f5858bc9172be67e22d6cd20
RedaRabouli <redarabouli@gmail.com>
2022-07-29 04:59:04 +0100

    pedro update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -27,9 +27,9 @@ next()
 
  exports.isAdmin = (req, res, next) => {
      if(req.auth.role == 0){
-         return res.status(403).json({
-             error: "Admin Resource, Access Denied!"
-         })
+         return res.status(403).json(   {error:"Admin Resource, Access Denied!"}                   )
      }
      next(); 
  }
+ 
+  
